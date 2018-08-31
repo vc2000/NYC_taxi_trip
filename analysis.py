846d04c9fb58ece9975f2dc5b98a46b53948b7b1
Venus Chau <vc2000@wildcats.unh.edu>
2018-08-31 11:45:41 -0400

    finish payments over 0

--- analysis.py
+++ analysis.py
@@ -4,9 +4,42 @@ from fractions import Fraction
 
 df = pd.read_csv("yellow_tripdata_2017-01.csv")
 
+"""
+    fraction of payments under $5 use a credit card
+"""
 print(
     "fraction of payments under $ 5 use a credit card : "
     + str(
         Fraction(df.query("total_amount < 5 & payment_type == 1").shape[0], df.shape[0])
     )
 )
+
+"""
+    Credit Card payments under $5 and a list sorted highest to lowest
+"""
+
+under_5_credit_df =df[(df.total_amount < 5) & (df.payment_type == 1)]
+
+under_5_credit_df.sort_values(by='total_amount', ascending=0)
+
+# print(under_5_credit_df.head())
+
+"""
+    fraction of payments over $50 use a credit card
+"""
+print(
+    "fraction of payments over $ 50 use a credit card : "
+    + str(
+        Fraction(df.query("total_amount > 50 & payment_type == 1").shape[0], df.shape[0])
+    )
+)
+
+"""
+    Number of credit card payments over $50
+"""
+print(
+    "Number of credit card payments over $50 "
+    + str( df.query("total_amount > 50 & payment_type == 1").shape[0])
+    )
+)
+
