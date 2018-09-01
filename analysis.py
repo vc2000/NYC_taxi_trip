e57e26082022f75286084ba77c9764fbe469fbb0
vc2000 <vc2000@wildcats.unh.edu>
2018-09-01 11:54:07 -0400

    fixed avg from JFK trip

--- analysis.py
+++ analysis.py
@@ -7,12 +7,12 @@ df = pd.read_csv("data/yellow_tripdata_2017-01.csv")
 """
     fraction of payments under $5 use a credit card
 """
-print(
+"""print(
     "fraction of payments under $ 5 use a credit card : "
     + str(
         Fraction(df.query("total_amount < 5 & payment_type == 1").shape[0], df.shape[0])
     )
-)
+)"""
 
 """
     Credit Card payments under $5 and a list sorted highest to lowest
@@ -59,7 +59,7 @@ print("saved" + file_name)"""
 df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
 df["time_diff"] = df["tpep_dropoff_datetime"] - df["tpep_pickup_datetime"]
 
-df["fare_per_min"] = df["fare_amount"] / df["time_diff"]
+df["fare_per_min"] = df["fare_amount"] / df["time_diff"] # wrong
 print(df.head())"""
 
 #print("the mean fare per minute driven :" + str( df["fare_per_min"].mean()))
@@ -79,19 +79,29 @@ print("the median of the taxi's fare per mile driven : " + str(df["fare_per_mile
     the 95 percentile of the taxi's average driving speed in miles per hour ??
 """
 
+
+
 """
     the average ratio of the distance between the pickup and drop-off divided by the distance driven???
 """
-# print(df.head())
+
+
+
+"""taxi_zone_df = pd.read_csv("data/taxi _zone_lookup.csv")
+
+
+result = pd.merge(df,taxi_zone_df, left_on="PULocationID", right_on="LocationID",how='left')
+print(result.head())"""
 
 # open taxi location file, get location geocode then find distance
 """
     the average tip for rides from JFK
 """
-
-"""JFK_avg = df[(df.RatecodeID = 2)
-
-print(JFK_avg.tip_amount.mean())"""
+"""taxi_zone_df = pd.read_csv("data/taxi _zone_lookup.csv")
+merge_loc_df = pd.merge(df,taxi_zone_df, left_on="PULocationID", right_on="LocationID",how='left')
+jfk= merge_loc_df.loc[merge_loc_df['Zone'] == 'JFK Airport']
+#jfk = merge_loc_df[merge_loc_df['Zone'].str.contains("JFK Airport")]
+print("the average tip for rides from JFK :"+str(jfk['tip_amount'].mean()))"""
 
 """
     the median March revenue of a taxi driver
@@ -99,5 +109,5 @@ print(JFK_avg.tip_amount.mean())"""
 """march_yellow_2017 = pd.read_csv("yellow_tripdata_2017-03.csv")
 
 march_yellow_2017["rev"] = march_yellow_2017['total_amount'] - march_yellow_2017['tolls_amount']
-"""
-# print("the median March revenue of a taxi driver :"+str(march_yellow_2017["rev"].median()))
+
+# print("the median March revenue of a taxi driver :"+str(march_yellow_2017["rev"].median()))"""
