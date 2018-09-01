0986c789246ff879f26de913556c6e380f5a18fa
vc2000 <vc2000@wildcats.unh.edu>
2018-08-31 22:05:45 -0400

    finished most of the questions

--- analysis.py
+++ analysis.py
@@ -74,6 +74,27 @@ df["fare_per_mile"] = df["fare_amount"] / df["trip_distance"]
 
 print("the median of the taxi's fare per mile driven : " + str(df["fare_per_mile"].median()) )"""
 
+
 """
-    the 95 percentile of the taxi's average driving speed in miles per hour
+    the 95 percentile of the taxi's average driving speed in miles per hour ??
 """
+
+"""
+    the average ratio of the distance between the pickup and drop-off divided by the distance driven???
+"""
+
+"""
+    the average tip for rides from JFK
+"""
+
+"""JFK_avg = df[(df.RatecodeID = 2)
+
+print(JFK_avg.tip_amount.mean())"""
+
+"""
+    the median March revenue of a taxi driver
+"""
+"""march_yellow_2017 = pd.read_csv("yellow_tripdata_2017-03.csv")
+
+march_yellow_2017["rev"] = march_yellow_2017['total_amount'] - march_yellow_2017['tolls_amount']
+print(march_yellow_2017.groupby(['VendorID'])['rev'].sum())"""
