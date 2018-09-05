65a9e93c1d52e1d850ffd9154e2a97ae1cccebf4
Venus Chau <vc2000@wildcats.unh.edu>
2018-09-05 09:25:20 -0400

    updated geocode

--- crawl_geocode.py
+++ crawl_geocode.py
@@ -3,9 +3,11 @@ import geocoder
 
 taxi_zone_df = pd.read_csv("data/taxi _zone_lookup.csv")
 
-#lat_lng = geocoder.google(x).latlng for x in taxi_zone_df.Zone
+taxi_zone_df['address']=taxi_zone_df['Borough'] + " , " + taxi_zone_df['Zone']
 
-taxi_zone_df["city_coord"] =[geocoder.google(x).latlng for x in taxi_zone_df.Zone]
+lat_lng = geocoder.google(x).latlng for x in taxi_zone_df.Zone
+
+taxi_zone_df["city_coord"] =[geocoder.google(x).latlng for x in taxi_zone_df.address]
 
 
 file_name = "zone_geocode.csv"
