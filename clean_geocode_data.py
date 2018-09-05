ea47ac1bd3274182f598e0da9a9cc4196531b4fd
Venus Chau <vc2000@wildcats.unh.edu>
2018-09-05 00:49:23 -0400

    clean null data

--- clean_geocode_data.py
+++ clean_geocode_data.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+import geocoder
+
 
 geo_df = pd.read_csv("zone_geocode.csv")
-#geo_df["city_coord"] = 
+geo_df["city_coord"] = [geo_df["city_coord"].fillna(geocoder.google(x).latlng for x in geo_df.Zone)]
 
-#print(geo_df["city_coord"])
 
-lat=[x[1] for x in np.asarray(geo_df["city_coord"])]
 
-print(lat.head())
+print(geo_df.head())
+
+file_name = "v2_zone_geocode.csv"
+geo_df.to_csv(file_name, sep=",", encoding="utf-8")
